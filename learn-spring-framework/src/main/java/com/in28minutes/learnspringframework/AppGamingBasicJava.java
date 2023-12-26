d14c527d75ace310405021651771e6a2790ed50a
Mingguriguri <rlaalswjd-@naver.com>
2023-12-26 18:53:10 +0900

    Section2 - step7 - Intro of Spring Framework

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -10,8 +10,12 @@ public class AppGamingBasicJava {
 	public static void main(String[] args) {
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
-		var game = new PacMan();
-		var gameRunner = new GameRunner(game);
+		
+		var game = new PacMan(); // 1: Object Creation
+		var gameRunner = new GameRunner(game); 
+			// 2: Object Creation + Wiring of Dependencies
+			// Game is Dependency of GameRunner class
+				// `Game`: MarioGame, SuperContraGame, GamingConsole...
 		gameRunner.run();
 	}
                                                                                                                                                                                                                                                                    
