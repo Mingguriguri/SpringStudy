83cf027789149f2abf02621ccb955dab0910ed3c
Mingguriguri <rlaalswjd-@naver.com>
2023-12-27 18:07:41 +0900

    Section2 - step9 - Creating more Java Spring Beans

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,14 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Eliminate verbosity in creating Java Beans
+//Public accessor methos, constructor,
+//equals, hashcode and toString are automatically created
+//Released in JDK 16.
+
+record Person (String name, int age) {};
+record Address (String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -10,5 +18,17 @@ public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
 		return "Minggu";
+	}
+	@Bean
+	public int age() {
+		return 15;
+	}
+	@Bean
+	public Person person(){
+		return new Person("Ravi", 20);
+	}
+	@Bean
+	public Address address(){
+		return new Address("Bakery Street", "London");
 	}
 }
