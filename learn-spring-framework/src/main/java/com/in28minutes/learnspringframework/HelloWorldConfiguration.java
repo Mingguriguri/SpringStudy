1daf126284ce1561549f1e56d91c42928f340a29
Mingguriguri <rlaalswjd-@naver.com>
2023-12-28 14:58:22 +0900

    Section2 - step10 - SpringFrameWorkJava에서 자동 연결 구현

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 //equals, hashcode and toString are automatically created
 //Released in JDK 16.
 
-record Person (String name, int age) {};
+record Person (String name, int age, Address address) {};
 record Address (String firstLine, String city) {};
 
 @Configuration
@@ -25,10 +25,25 @@ public class HelloWorldConfiguration {
 	}
 	@Bean
 	public Person person(){
-		return new Person("Ravi", 20);
+		return new Person("Ravi", 20, new Address("Main Street", "Uthrecht"));
 	}
+	
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); // name, age
+	}
+	
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) { // name, age, address2
+		return new Person(name, age, address3); 
+	}
+	
+	@Bean(name = "address2")
 	public Address address(){
 		return new Address("Bakery Street", "London");
+	}
+	@Bean(name = "address3")
+	public Address address3(){
+		return new Address("Motinagar", "HGyderabad");
 	}
 }
