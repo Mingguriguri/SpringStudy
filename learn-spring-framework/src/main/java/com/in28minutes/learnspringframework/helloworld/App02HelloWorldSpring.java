357197c16c4578261c140871f3ac4db8c3aa82b3
Mingguriguri <rlaalswjd-@naver.com>
2024-01-01 23:07:59 +0900

    Section2 - step 15. Try-with-services

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -13,26 +13,26 @@ public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
 		// 1: - Use @Configuration -> Launch a Spring Context 
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-				
-		// 2: Configure the things that we want Spring to manage - @Configuration
-		// by creating HelloWorldConfiguration class - @Configuration
-		// name(메소드명) - @Bean
-		
-		// 3: Retrieving Beans managed by Spring
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("person2MethodCall"));
-		System.out.println(context.getBean("person3Parameters"));
-		System.out.println(context.getBean("address2"));
-		System.out.println(context.getBean(Person.class));
-		System.out.println(context.getBean(Address.class));
-		System.out.println(context.getBean("person5Qualifier"));
-		//System.out.println
-		//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-		
-		//context.getBeanDefinitionCount();
-
+		try(var context = new AnnotationConfigApplicationContext
+								(HelloWorldConfiguration.class)){
+			// 2: Configure the things that we want Spring to manage - @Configuration
+			// by creating HelloWorldConfiguration class - @Configuration
+			// name(메소드명) - @Bean
+			
+			// 3: Retrieving Beans managed by Spring
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("person2MethodCall"));
+			System.out.println(context.getBean("person3Parameters"));
+			System.out.println(context.getBean("address2"));
+			System.out.println(context.getBean(Person.class));
+			System.out.println(context.getBean(Address.class));
+			System.out.println(context.getBean("person5Qualifier"));
+			//System.out.println
+			//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			
+			//context.getBeanDefinitionCount();
+		}	
 	}                                                                                                                                                                                                                                                           
 }
