39dc674b61e4f76464a0d6e76750b57ff21b931d
Mingguriguri <rlaalswjd-@naver.com>
2024-01-02 03:19:19 +0900

    Section3 - step1 - Basic Setting2

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacMan.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacMan implements GamingConsole {
+	public void up() {
+		System.out.println("up");
+	}
+	public void down() {
+		System.out.println("down");
+	}
+	public void left() {
+		System.out.println("left");
+	}
+	public void right() {
+		System.out.println("right");
+	}
+}
