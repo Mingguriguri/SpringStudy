4c08c2c372519a116fd8dfd527d5b7deb968f126
Mingguriguri <rlaalswjd-@naver.com>
2024-01-02 03:21:09 +0900

    Section3 - step1 - Basic Setting3

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,6 +1,8 @@
 package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GamingConsole;
@@ -8,14 +10,24 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PacMan;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
+@Configuration
 public class App03GamingSpringBeans {
-
+	@Bean
+	public GamingConsole game() {
+		var game = new PacMan();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game); 
+		return gameRunner;
+	}
 	public static void main(String[] args) {
 		try(var context = new AnnotationConfigApplicationContext
-							(GamingConfiguration.class)){
+							(App03GamingSpringBeans.class)){
 			
 			context.getBean(GamingConsole.class).up();
-			
 			context.getBean(GameRunner.class).run();
 		}
 	}                                                                                                                                                                                                                                                                
