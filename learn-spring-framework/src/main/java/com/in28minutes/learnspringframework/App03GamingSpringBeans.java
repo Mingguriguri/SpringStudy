8079098090b45185d1978ec9932eb9c1c9a90bfe
Mingguriguri <rlaalswjd-@naver.com>
2024-01-01 23:34:22 +0900

    Section2 - step 15. Spring 프레임워크를 사용하여 Java Gaming 앱 관리

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -0,0 +1,22 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacMan;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+public class App03GamingSpringBeans {
+
+	public static void main(String[] args) {
+		try(var context = new AnnotationConfigApplicationContext
+							(GamingConfiguration.class)){
+			
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+		}
+	}                                                                                                                                                                                                                                                                
+}
---
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/GamingConfiguration.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PacMan;
+
+@Configuration
+public class GamingConfiguration {
+	
+	@Bean
+	public GamingConsole game() {
+		var game = new PacMan();
+		return game;
+	}
+	
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game); 
+		return gameRunner;
+	}
+}
