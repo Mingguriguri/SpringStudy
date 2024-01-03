548bb244706d408ee57d10b214d38e0e659c1960
Mingguriguri <rlaalswjd-@naver.com>
2024-01-04 01:32:24 +0900

    Section3 - step4 - Dependency Injection(2). Setter Injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -11,10 +11,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 class YourBusinessClass{
-	@Autowired
+	
 	Dependency1 dependency1;
-	@Autowired
 	Dependency2 dependency2;
+
+	@Autowired
+	public void setDependency1(Dependency1 dependency1) {
+		System.out.println("Setter Injection - setDependency1");
+		this.dependency1 = dependency1;
+	}
+	
+	@Autowired
+	public void setDependency2(Dependency2 dependency2) {
+		System.out.println("Setter Injection - setDependency2");
+		this.dependency2 = dependency2;
+	}
 	
 	public String toString() {
 		return "Using: "+ dependency1 + " + " + dependency2;
