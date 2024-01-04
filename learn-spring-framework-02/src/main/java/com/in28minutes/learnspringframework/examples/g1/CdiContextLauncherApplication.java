b6f3978f21fdbcfd3c38d67cf2c8fbea938a17ac
Mingguriguri <rlaalswjd-@naver.com>
2024-01-05 02:19:18 +0900

    Section4 - step 7 - CDI

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/g1/CdiContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/g1/CdiContextLauncherApplication.java
@@ -9,23 +9,31 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+//@Component
+@Named
 class BusinessService {
 
 	private DataService dataService;
 
-	@Autowired
-	public DataService getDataService() {
-		System.out.println("Setter Injection");
-		return dataService;
-	}
-
+	//@Autowired
+	@Inject
 	public void setDataService(DataService dataService) {
+		System.out.println("Setter Injection");
 		this.dataService = dataService;
 	}
+	
+	public DataService getDataService() {
+		return dataService;
+	}
+	
+	
 }
 
-@Component
+//@Component
+@Named
 class DataService {
 	
 }
@@ -37,6 +45,8 @@ public class CdiContextLauncherApplication {
 							(CdiContextLauncherApplication.class)){
 			Arrays.stream(context.getBeanDefinitionNames()).
 			forEach(System.out::println);
+			
+			System.out.println(context.getBean(BusinessService.class).getDataService());
 			
 		}
 	}                                                                                                                                                                                                                                                                
