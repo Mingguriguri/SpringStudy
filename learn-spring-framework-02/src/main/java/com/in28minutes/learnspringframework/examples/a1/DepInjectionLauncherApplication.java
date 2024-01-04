cfb2502057e4a26df8df25664539bab214d31fc3
Mingguriguri <rlaalswjd-@naver.com>
2024-01-04 09:48:59 +0900

    Section3 - step4 - Dependency Injection(3). Constructor Injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -14,18 +14,28 @@ class YourBusinessClass{
 	
 	Dependency1 dependency1;
 	Dependency2 dependency2;
-
-	@Autowired
-	public void setDependency1(Dependency1 dependency1) {
-		System.out.println("Setter Injection - setDependency1");
-		this.dependency1 = dependency1;
-	}
 	
-	@Autowired
-	public void setDependency2(Dependency2 dependency2) {
-		System.out.println("Setter Injection - setDependency2");
-		this.dependency2 = dependency2;
-	}
+	
+	//@Autowired
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+			super();
+			System.out.println("Constructor Injection - YourBusinessClass");
+			this.dependency1 = dependency1;
+			this.dependency2 = dependency2;
+		}
+
+
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection - setDependency1");
+//		this.dependency1 = dependency1;
+//	}
+//	
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection - setDependency2");
+//		this.dependency2 = dependency2;
+//	}
 	
 	public String toString() {
 		return "Using: "+ dependency1 + " + " + dependency2;
