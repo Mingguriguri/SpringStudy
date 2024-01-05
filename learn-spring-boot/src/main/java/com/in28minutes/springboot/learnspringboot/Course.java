065b971cb151482878325beb112b7dd0f57e718d
Mingguriguri <rlaalswjd-@naver.com>
2024-01-05 21:09:09 +0900

    Section5 - step4 - using Spring Boot, build HelloWorld API

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/Course.java
@@ -0,0 +1,44 @@
+package com.in28minutes.springboot.learnspringboot;
+
+public class Course {
+	private long id;
+	private String name;
+	private String author;
+	
+	// Constructor
+	public Course(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+	
+	// Getters	
+	
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	
+
+	//toString
+	
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+
+	
+	
+	
+	
+	
+	
+}
---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class CourseController {
+
+	// URL: http://localhost:8080/courses
+	// Course detail: id, name, author
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "in28minutes"),
+				new Course(2, "Learn DevOps", "in28minutes")
+				
+			);
+	}
+}
