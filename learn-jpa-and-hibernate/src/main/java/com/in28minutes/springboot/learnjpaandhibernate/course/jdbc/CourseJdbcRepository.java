2b7d56dbc4044d84141d2216180bbbaca5ac51b2
Mingguriguri <rlaalswjd-@naver.com>
2024-01-09 16:44:04 +0900

    Section6 - step 4 - Spring JDBC, simple data insertion

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -0,0 +1,22 @@
+package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseJdbcRepository {
+	
+	@Autowired
+	private JdbcTemplate springJdbcTemplate;
+	
+	private static String INSERT_QUERY = 
+			"""
+			insert into course (id, name, author)
+			values (1, 'learn AWS', 'in28minutes');
+			""";
+	public void insert() {
+		springJdbcTemplate.update(INSERT_QUERY);
+	}
+	
+}
