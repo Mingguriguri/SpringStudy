5260104f614993b9d586f9f2b80ea2ba1da71563
Mingguriguri <rlaalswjd-@naver.com>
2024-01-21 23:36:45 +0900

    [Section8] - 5. @PathVariable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,15 @@ public class HellowWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	// Path Parameters
+	// /users/{id}/todos/{id} => /users/1/todos/101
+	// hello-world/path-variable/{name}
+	
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(
+				String.format("Hello World, %s", name));
 	}
 }
