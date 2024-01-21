e6e3e1caf3674587d1934a89a52c4010bb716ea5
Mingguriguri <rlaalswjd-@naver.com>
2024-01-21 21:17:17 +0900

    [Section8] - 2. Hello World Controller (REST API)

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
@@ -1,10 +1,12 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// REST API
+@RestController
 public class HellowWorldController {
-
-	// url : hello-world
-	
-	// return "Hello World"
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
 }
