83fd394aedc1e6597c9148e33ea1f47e6e78e5ef
Mingguriguri <rlaalswjd-@naver.com>
2024-01-21 21:49:54 +0900

    [Section8] - 3. Hello World Bean

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HellowWorldController.java
@@ -9,4 +9,9 @@ public class HellowWorldController {
 	public String helloWorld() {
 		return "Hello World";
 	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
 }
