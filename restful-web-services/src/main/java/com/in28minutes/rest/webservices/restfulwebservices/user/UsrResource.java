803697f4e2ae759e3c5e294f325baf699b32b4bf
Mingguriguri <rlaalswjd-@naver.com>
2024-01-22 21:05:07 +0900

    [Section8] 10. HTTP 상태 코드와 location

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UsrResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UsrResource.java
@@ -1,12 +1,15 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UsrResource {
@@ -31,7 +34,14 @@ public class UsrResource {
 	
 	// POST /users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		// /users/{id} => 즉, user.getId()
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+							.path("/{id}")
+							.buildAndExpand(savedUser.getId())
+							.toUri();
+		return ResponseEntity.created(location).build();
 	}
+	
 }
