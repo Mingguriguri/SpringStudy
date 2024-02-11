4cde3236d9b152803677bd9f4e951870e1699b08
Mingguriguri <rlaalswjd-@naver.com>
2024-02-11 16:53:01 +0900

    [Section20] 8. Spring Boot Maven Plugin - Create Docker Image (v5)

--- master-spring-and-spring-boot-main/83-docker/hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ master-spring-and-spring-boot-main/83-docker/hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
     public String helloWorld() {
 		// Implemented using String Templates
         return """
-                { message: Hello World Java v4 }
+                { message: Hello World Java v5 }
                """;
     }
 }
