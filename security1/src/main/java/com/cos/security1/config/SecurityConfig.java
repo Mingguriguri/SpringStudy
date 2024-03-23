3189764b1386047235dddc46ec37c42e7acd1d7d
Mingguriguri <rlaalswjd-@naver.com>
2024-03-23 22:24:08 +0900

    Docs(Spring Security): 라인별로 주석 추가

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -16,10 +16,10 @@ public class SecurityConfig {
 		
 		http
 		.authorizeRequests()
-			.requestMatchers("/user/**").authenticated() 
-			.requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-			.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
-			.anyRequest().permitAll();
+			.requestMatchers("/user/**").authenticated() // authenticated(): 인증 완료해야 접근 가능
+			.requestMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") //인증 후, admin이나 manager권한 있어야 함
+			.requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") //인증 후, admin이나 manager권한 있어야 함
+			.anyRequest().permitAll(); // anyRequest(): 그 외 나머지 리소스들, permitAll(): 설정한 리소스의 접근을 인증절차 없이 허용한다는 의미
 
 		return http.build();
 	}
