6c67c51de128adae49dcd420e83dd68de41b5d4b
Mingguriguri <rlaalswjd-@naver.com>
2024-03-31 11:04:04 +0900

    [Section16] Spring Security - step12 - JDBC를 사용해 사용자 자격증명 저장

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -1,12 +1,17 @@
 package com.in28minutes.learnspringsecurity.basic;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -32,24 +37,56 @@ public class BasicAuthSecurityConfiguration {
 		
 		http.csrf(csrf -> csrf.disable());
 		
+		// h2-console
+		http.headers().frameOptions().sameOrigin();
 		return http.build();
 	}
 
+//	@Bean
+//	public UserDetailsService userDetailService() {
+//		
+//		var user = User.withUsername("in28minutes")
+//			.password("{noop}dummy")
+//			.roles("USER")
+//			.build();
+//
+//		
+//		var admin = User.withUsername("admin")
+//				.password("{noop}dummy")
+//				.roles("ADMIN")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+	
 	@Bean
-	public UserDetailsService userDetailService() {
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailService(DataSource dataSource) {
 		
 		var user = User.withUsername("in28minutes")
 			.password("{noop}dummy")
 			.roles("USER")
 			.build();
-
 		
 		var admin = User.withUsername("admin")
 				.password("{noop}dummy")
-				.roles("ADMIN")
+				.roles("ADMIN", "USER")
 				.build();
+		
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
 
-		return new InMemoryUserDetailsManager(user, admin);
+		return jdbcUserDetailsManager;
 	}
+
+
 	
 }
