ef641739f650dfb74fe8a32ba1f76c698569102a
Mingguriguri <rlaalswjd-@naver.com>
2024-03-31 09:37:46 +0900

    [Section16] Spring Security - step11. InMemory - 사용자 자격증명

--- learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -32,5 +35,21 @@ public class BasicAuthSecurityConfiguration {
 		return http.build();
 	}
 
+	@Bean
+	public UserDetailsService userDetailService() {
+		
+		var user = User.withUsername("in28minutes")
+			.password("{noop}dummy")
+			.roles("USER")
+			.build();
 
+		
+		var admin = User.withUsername("admin")
+				.password("{noop}dummy")
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
 }
